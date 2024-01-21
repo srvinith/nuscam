38b56f55ca079665fb0c4f5aaa97cf5476cd3772
srvinith <r36041595@gmail.com>
2024-01-21 12:33:34 +0530

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
+// calanedar
+
 var Cal = function(divId) {
 
-    //Store div id
     this.divId = divId;
-  
-    // Days of week, starting on Sunday
+
     this.DaysOfWeek = [
       'S',
       'M',
@@ -13,11 +13,9 @@ var Cal = function(divId) {
       'F',
       'S'
     ];
-  
-    // Months, stating on January
+ 
     this.Months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
-  
-    // Set the current month, year
+
     var d = new Date();
   
     this.currMonth = d.getMonth();
@@ -26,7 +24,7 @@ var Cal = function(divId) {
   
   };
   
-  // Goes to next month
+
   Cal.prototype.nextMonth = function() {
     if ( this.currMonth == 11 ) {
       this.currMonth = 0;
@@ -38,7 +36,7 @@ var Cal = function(divId) {
     this.showcurr();
   };
   
-  // Goes to previous month
+ 
   Cal.prototype.previousMonth = function() {
     if ( this.currMonth == 0 ) {
       this.currMonth = 11;
@@ -59,41 +57,34 @@ var Cal = function(divId) {
   Cal.prototype.showMonth = function(y, m) {
   
     var d = new Date()
-    // First day of the week in the selected month
     , firstDayOfMonth = new Date(y, m, 1).getDay()
-    // Last day of the selected month
     , lastDateOfMonth =  new Date(y, m+1, 0).getDate()
-    // Last day of the previous month
     , lastDayOfLastMonth = m == 0 ? new Date(y-1, 11, 0).getDate() : new Date(y, m, 0).getDate();
-  
-  
+
     var html = '<table>';
   
-    // Write selected month and year
+
     html += '<thead><tr>';
     html += '<td colspan="8">' + this.Months[m] + ' ' + y + '</td>';
     html += '</tr></thead>';
   
   
-    // Write the header of the days of the week
     html += '<tr class="days">';
     for(var i=0; i < this.DaysOfWeek.length;i++) {
       html += '<td>' + this.DaysOfWeek[i] + '</td>';
     }
     html += '</tr>';
   
-    // Write the days
     var i=1;
     do {
   
       var dow = new Date(y, m, i).getDay();
   
-      // If Sunday, start new row
+     
       if ( dow == 0 ) {
         html += '<tr>';
       }
-      // If not Sunday but first day of the month
-      // it will write the last days from the previous month
+     
       else if ( i == 1 ) {
         html += '<tr>';
         var k = lastDayOfLastMonth - firstDayOfMonth+1;
@@ -103,7 +94,6 @@ var Cal = function(divId) {
         }
       }
   
-      // Write the current day in the loop
       var chk = new Date();
       var chkY = chk.getFullYear();
       var chkM = chk.getMonth();
@@ -112,12 +102,11 @@ var Cal = function(divId) {
       } else {
         html += '<td class="normal">' + i + '</td>';
       }
-      // If Saturday, closes the row
+  
       if ( dow == 6 ) {
         html += '</tr>';
       }
-      // If not Saturday, but last day of the selected month
-      // it will write the next few days from the next month
+     
       else if ( i == lastDateOfMonth ) {
         var k=1;
         for(dow; dow < 6; dow++) {
@@ -129,22 +118,16 @@ var Cal = function(divId) {
       i++;
     }while(i <= lastDateOfMonth);
   
-    // Closes table
+    
     html += '</table>';
-  
-    // Write HTML to the div
     document.getElementById(this.divId).innerHTML = html;
   };
   
-  // On Load of the window
   window.onload = function() {
   
-    // Start calendar
     var c = new Cal("divCal");			
     c.showcurr();
-  
-    // Bind next and previous button clicks
-    getId('btnNext').onclick = function() {
+      getId('btnNext').onclick = function() {
       c.nextMonth();
     };
     getId('btnPrev').onclick = function() {
@@ -152,9 +135,8 @@ var Cal = function(divId) {
     };
   }
   
-  // Get element by id
   function getId(id) {
     return document.getElementById(id);
   }
 
-//   chart
+
